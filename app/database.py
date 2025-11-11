b3686e4ff665007fa317270d6d35d943dc66ce7b
Guilherme Da Gama Pimenta <guilherme44pimenta@gmail.com>
2025-11-11 19:18:30 -0300

    Versão inicial do projeto de API de inventário com Postgresql e docker

--- app/database.py
+++ app/database.py
@@ -1,31 +1,27 @@
 import os
 from sqlmodel import SQLModel
-# ESTA É A LINHA CORRIGIDA:
 from sqlalchemy.ext.asyncio import create_async_engine 
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from dotenv import load_dotenv
 
-# Carrega as variáveis de ambiente (do docker-compose)
+# serve para carregar as variáveis de ambiente definidas no docker-compose
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Criamos o "motor" (engine) assíncrono
-# O 'echo=True' é ótimo para debug, pois mostra os comandos SQL no terminal
+# A função desse 'echo=True' é para mostrar os comandos SQL no terminal (Gabriel disse que ajuda na hora de debugar)
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-# Função para criar as tabelas no banco (se não existirem)
+# Função para criar as tabelas no banco (caso não existam)
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        # 'metadata.create_all' cria as tabelas
         await conn.run_sync(SQLModel.metadata.create_all)
 
-# Função para obter uma sessão (conexão) com o banco
+# Função para obter uma sessão/conexão com o banco
 async def get_session():
-    # Cria uma fábrica de sessões
+    # Cria um 'fazedor' de sessões (nao sei as melhores palavras pra explicar isso)
     async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     async with async_session() as session:
-        # 'yield' entrega a sessão para a função que a pediu
-        # e garante que ela será fechada no final, mesmo se der erro.
+        # O yield entrega a sessão para a função que a pediu e garante que ela será fechada no final, mesmo que de erro.
         try:
             yield session
         finally:
--- app/models.py
+++ app/models.py
@@ -1,15 +1,11 @@
 from sqlmodel import Field, SQLModel
 from typing import Optional
 
-# SQLModel é ao mesmo tempo um modelo Pydantic e um modelo SQLAlchemy
 class ItemInventario(SQLModel, table=True):
-    # O 'id' agora será gerenciado pelo banco (Primary Key)
+    # Desse modo, o 'id' será gerenciado pelo bd
     id: Optional[int] = Field(default=None, primary_key=True)
     
-    nome: str = Field(index=True) # index=True melhora a velocidade de busca
+    nome: str = Field(index=True) # index=True -> a indexação melhora a velocidade dd busca
     descricao: Optional[str] = None
     preco: float = Field(gt=0)
     quantidade: int = Field(ge=0)
-
-# Não precisamos mais de um modelo de 'Resposta' separado,
-# o SQLModel já lida com a criação (sem id) e resposta (com id).
